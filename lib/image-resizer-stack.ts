fb6c88901d6b2e6781eec5f3efc62e74e2be5917
TOCHI <don@DON-3.local>
2022-09-18 13:35:38 -0400

    init

--- lib/image-resizer-stack.ts
+++ lib/image-resizer-stack.ts
@@ -1,16 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as aws_s3 from 'aws-cdk-lib/aws-s3'
+import * as s3_deploy from 'aws-cdk-lib/aws-s3-deployment';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 export class ImageResizerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const website = new aws_s3.Bucket(this, 'Bucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "error.html",
+      removalPolicy: RemovalPolicy.DESTROY
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImageResizerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, "static-website-url", {
+      value: website.bucketWebsiteUrl,
+      description: "Bucket Website Url"
+    })
+
+    new s3_deploy.BucketDeployment(this, 'static-website-deploy', {
+      sources: [s3_deploy.Source.asset('./website-dist')],
+      destinationBucket: website
+    });
+
+    new lambda.Function(this, "image-resizer", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda/Image-resizer'),
+      handler: 'imageResizer.handler',
+      environment: {
+
+      }
+    })
+
   }
 }
