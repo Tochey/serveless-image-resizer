b32a26ce02f272e9449de9733e11da9bdf5fc2bf
TOCHI <don@DON-3.local>
2022-09-19 03:02:07 -0400

    added pipeline

--- lib/image-resizer-pipeline-stack.ts
+++ lib/image-resizer-pipeline-stack.ts
@@ -1,13 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit'
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 export class ImageResizerStackPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new codecommit.Repository(this, 'ImageResizer', {
+        const repository = new codecommit.Repository(this, 'ImageResizer', {
             repositoryName: "image-resizer"
         });
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'ImageResizerPipeline',
+            synth: new CodeBuildStep('SynthStep', {
+                input: CodePipelineSource.codeCommit(repository, 'main'),
+                installCommands: [
+                    'npm install -g aws-cdk'
+                ],
+                commands: [
+                    'npm ci',
+                    'npm build',
+                    'npx cdk synth'
+                ]
+            })
+        })
 
     }
 }
