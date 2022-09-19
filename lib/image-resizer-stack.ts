1e59be5d58d2e7d09ea0d54f4860fddd6e018f3e
TOCHI <don@DON-3.local>
2022-09-18 23:27:52 -0400

    added architecture diagram

--- lib/image-resizer-stack.ts
+++ lib/image-resizer-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as aws_s3 from 'aws-cdk-lib/aws-s3'
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
 import * as s3_deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { RemovalPolicy } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
@@ -9,31 +10,46 @@ export class ImageResizerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const website = new aws_s3.Bucket(this, 'Bucket', {
+    const website = new aws_s3.Bucket(this, 'websiteBucket', {
       publicReadAccess: true,
       websiteIndexDocument: "index.html",
       websiteErrorDocument: "error.html",
       removalPolicy: RemovalPolicy.DESTROY
     })
 
-    new cdk.CfnOutput(this, "static-website-url", {
-      value: website.bucketWebsiteUrl,
-      description: "Bucket Website Url"
-    })
 
     new s3_deploy.BucketDeployment(this, 'static-website-deploy', {
       sources: [s3_deploy.Source.asset('./website-dist')],
-      destinationBucket: website
+      destinationBucket: website,
+      retainOnDelete : false
     });
 
-    new lambda.Function(this, "image-resizer", {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('lambda/Image-resizer'),
-      handler: 'imageResizer.handler',
-      environment: {
+    new cdk.CfnOutput(this, "static-website-url", {
+      value: website.bucketWebsiteUrl,
+      description: "Bucket Website Url"
+      
+      
+    })
+    const uploadBucket = new aws_s3.Bucket(this, 'userUploadBucket', {
+      removalPolicy: RemovalPolicy.DESTROY
+    })
 
+      const imageWorker = new lambda.Function(this, "image-worker", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda/imageworker'),
+      handler: 'imageWorker.handler',
+      environment: {
+        s3_UPLOAD_BUCKET : uploadBucket.bucketName
       }
     })
 
+    const api = new apigw.LambdaRestApi(this, 'getsignedUrl', {
+      handler: imageWorker,
+      restApiName : 'getSignedUrl',
+      proxy : false
+    })
+
+    const s3SignedUrl = api.root.addResource('getSignedUrl')
+    s3SignedUrl.addMethod('GET',  new apigw.LambdaIntegration(imageWorker))
   }
 }
